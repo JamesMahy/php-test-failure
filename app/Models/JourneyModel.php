bacb58e9589821172e4d9098dd9cf6007e22cf97
James Mahy <james.mahy@cevo.co.uk>
2019-05-08 19:33:42 +0100

    Moved the handler function out of the model, added comments

--- app/Models/JourneyModel.php
+++ app/Models/JourneyModel.php
@@ -19,11 +19,22 @@ class JourneyModel extends SessionModel {
 		if(!isset($_SESSION['credit'])){$_SESSION['credit'] = 0.00;}
 	}
 	
+	/***
+	 * Adds the $amount to the balance
+	 * @param Float $amount
+	 * @return Float
+	 */
 	public function topupCard(Float $amount = 0.00){
 		$_SESSION['credit'] += $amount;
 		return $this->getCardAmount();
 	}
 	
+	/***
+	 * Deducts the $amount from the balance
+	 * @param Float $amount
+	 * @return Float
+	 * @throws \Exception
+	 */
 	public function deductFromCard(Float $amount = 0.00){
 		if($_SESSION['credit'] - $amount >= 0){
 			$_SESSION['credit'] -= $amount;
@@ -35,101 +46,11 @@ class JourneyModel extends SessionModel {
 		throw new \Exception("You don't have enough credit to complete this transaction, please top-up by at-least &pound;".number_format($difference,2));
 	}
 	
+	/***
+	 * Returns the balance
+	 * @return Float
+	 */
 	public function getCardAmount(){
-		return $_SESSION['credit'];
+		return floatval($_SESSION['credit']);
 	}
-	
-	public function handleJourney(Array $data=[]){
-		
-		$mode = $data['transport_mode'] ?? 0;
-		$fare = 0.00;
-		
-		$config = \HMPP\Bootstrap::getInstance()->make("\HMPP\Core\Config");
-		
-		$journeyData = array(
-				"mode" => $mode,
-				"date" => date("Y-m-d"),
-				"time" => date("H:i")
-		);
-		
-		if($mode == 1){
-			$fare = $config->get("zone_fares\\bus");
-			
-			if(!isset($data['bus_destination']) || empty($data['bus_destination'])){
-				throw new \Exception("You didn't tell us where you'd like to take the bus!");
-			}
-			
-			$journeyData['to'] = $data['bus_destination'];
-		}
-		else{
-			
-			$start = $data['destination_from'] ?? 0;
-			$finish = $data['destination_to'] ?? 0;
-			
-			if(empty($start) || empty($finish)){
-				throw new \Exception("Please provide a destination from and to");
-			}
-			else if($start == $finish){
-				throw new \Exception("You can't travel to the same location silly");
-			}else {
-				$journeyData['from'] = $data['destination_from'];
-				$journeyData['to'] = $data['destination_to'];
-				
-				$from = $config->get("destinations\\" . $start);
-				$to = $config->get("destinations\\" . $finish);
-				
-				$zones = array_merge($from, $to);
-				sort($zones);
-				
-				
-				$minFrom = min($from);
-				$minTo = min($to);
-				
-				$maxFrom = max($from);
-				$maxTo = max($to);
-				
-				$numberOfZones = 0;
-				
-				if ($minFrom != 1 || $minTo != 1){
-					if ($maxFrom > $maxTo) {
-						$numberOfZones = $maxFrom - $maxTo;
-					} elseif ($maxFrom < $maxTo) {
-						$numberOfZones = $maxTo - $maxFrom;
-					}
-				}
-				
-				$numberOfZones++;
-				
-				if($numberOfZones){
-					if($numberOfZones == 1){
-						if($minFrom == 1 && $minTo == 1){
-							$fare = $config->get("zone_fares\\zone_1");
-						}else{
-							$fare = $config->get("zone_fares\\outside_zone_1");
-						}
-					}
-					else if($numberOfZones == 2){
-						if($minFrom == 1 || $minTo == 1){
-							$fare = $config->get("zone_fares\\two_zones_inc_zone_1");
-						}else{
-							$fare = $config->get("zone_fares\\two_zones_exc_zone_1");
-						}
-					}else{
-						$fare = $config->get("max_fare");
-					}
-				}
-			}
-			
-		}
-		
-		
-		$this->deductFromCard($fare);
-		
-		$journeyData['fare'] = $fare;
-		$this->set($journeyData);
-		
-		return $journeyData;
-		
-	}
-	
 }
--- app/public/index.php
+++ app/public/index.php
@@ -32,7 +32,7 @@ class Index extends \HMPP\Core\Framework {
 				}
 			}else{
 				try{
-					$journeyData = $model->handleJourney($_POST);
+					$journeyData = $this->handleJourney($_POST);
 					return 	Header("Location: /?message=Welcome to ".$journeyData['to']."&".http_build_query($_POST));
 				}catch (\Exception $e){
 					$error = $e->getMessage();
@@ -124,6 +124,105 @@ class Index extends \HMPP\Core\Framework {
 				),
 				"Let's go somewhere fun!"
 		);
+	}
+	
+	/***
+	 * Handles the journey post and saves the information to the database.
+	 * @param array $data
+	 * @return array
+	 * @throws \Exception
+	 */
+	public function handleJourney(Array $data=[]){
+		
+		$mode = $data['transport_mode'] ?? 0;
+		$fare = 0.00;
+		
+		$config = $this->make("\HMPP\Core\Config");
+		
+		$journeyData = array(
+				"mode" => $mode,
+				"date" => date("Y-m-d"),
+				"time" => date("H:i")
+		);
+		
+		if($mode == 1){
+			$fare = $config->get("zone_fares\\bus");
+			
+			if(!isset($data['bus_destination']) || empty($data['bus_destination'])){
+				throw new \Exception("You didn't tell us where you'd like to take the bus!");
+			}
+			
+			$journeyData['to'] = $data['bus_destination'];
+		}
+		else{
+			
+			$start = $data['destination_from'] ?? 0;
+			$finish = $data['destination_to'] ?? 0;
+			
+			if(empty($start) || empty($finish)){
+				throw new \Exception("Please provide a destination from and to");
+			}
+			else if($start == $finish){
+				throw new \Exception("You can't travel to the same location silly");
+			}else {
+				$journeyData['from'] = $data['destination_from'];
+				$journeyData['to'] = $data['destination_to'];
+				
+				$from = $config->get("destinations\\" . $start);
+				$to = $config->get("destinations\\" . $finish);
+				
+				$zones = array_merge($from, $to);
+				sort($zones);
+				
+				
+				$minFrom = min($from);
+				$minTo = min($to);
+				
+				$maxFrom = max($from);
+				$maxTo = max($to);
+				
+				$numberOfZones = 0;
+				
+				if ($minFrom != 1 || $minTo != 1){
+					if ($maxFrom > $maxTo) {
+						$numberOfZones = $maxFrom - $maxTo;
+					} elseif ($maxFrom < $maxTo) {
+						$numberOfZones = $maxTo - $maxFrom;
+					}
+				}
+				
+				$numberOfZones++;
+				
+				if($numberOfZones){
+					if($numberOfZones == 1){
+						if($minFrom == 1 && $minTo == 1){
+							$fare = $config->get("zone_fares\\zone_1");
+						}else{
+							$fare = $config->get("zone_fares\\outside_zone_1");
+						}
+					}
+					else if($numberOfZones == 2){
+						if($minFrom == 1 || $minTo == 1){
+							$fare = $config->get("zone_fares\\two_zones_inc_zone_1");
+						}else{
+							$fare = $config->get("zone_fares\\two_zones_exc_zone_1");
+						}
+					}else{
+						$fare = $config->get("max_fare");
+					}
+				}
+			}
+			
+		}
+		
+		$model = $this->make("\HMPP\Models\JourneyModel");
+		$model->deductFromCard($fare);
+		
+		$journeyData['fare'] = $fare;
+		$model->set($journeyData);
+		
+		return $journeyData;
+		
 	}
 }
 // Launch index
